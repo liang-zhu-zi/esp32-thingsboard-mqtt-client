f68ea3c9c72b8c5905f1756e1025c2fe42d107b8
liangzhuzi2020 <liangzhuzi2020@gmail.com>
2022-08-05 18:08:48 +0800

    remove some un-uesd files, rename some files

--- component.mk
+++ component.mk
@@ -3,7 +3,8 @@
 #
 
 COMPONENT_SRCDIRS := src
-ifdef CONFIG_ESP_IPC_ISR_ENABLE
-    COMPONENT_SRCDIRS += src/esp_ipc_isr
-endif
+# ifdef CONFIG_ESP_IPC_ISR_ENABLE
+#    COMPONENT_SRCDIRS += src/esp_ipc_isr
+# endif
 COMPONENT_ADD_INCLUDEDIRS := include
+# COMPONENT_PRIV_INCLUDEDIRS := interface
